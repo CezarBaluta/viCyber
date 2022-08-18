6a5872879ef58b284d71234566052eeb2fbb568e
CezarBaluta <cezar.baluta@yahoo.com>
2022-08-19 02:32:17 +0300

    navbar update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+  showMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "0";
+  }
+  
+  hideMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "-200px";
+  }
+  
+  showLogo1(){
+    document.getElementsByClassName("sponsors")[1].classList.remove('mobile-sponsors');
 
-  ngOnInit(): void {
+    console.log(document.getElementsByClassName("sponsors"));
+    var logo = document.getElementsByClassName("sponsors")[0];
+    logo.classList.add('mobile-sponsors');
+    console.log(logo.classList);
+    setTimeout(() => this.showLogo2(),3000)
+    
+    
+  }
+   showLogo2(){
+    document.getElementsByClassName("sponsors")[0].classList.remove('mobile-sponsors');
+    var logo = document.getElementsByClassName("sponsors")[1];
+    logo.classList.add('mobile-sponsors');
+    setTimeout(() => this.showLogo1(),3000)
   }
 
+  ngOnInit(): void {
+    this.showLogo1();
+  }
+  
 }
+
+
